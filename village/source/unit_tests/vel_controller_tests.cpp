a7a96ea209fb785eaad7546622a06633c74362db
Juliet Colman <juliet@amjula.com>
2023-01-30 10:58:57 +0000

    Fix up tests

--- village/source/unit_tests/vel_controller_tests.cpp
+++ village/source/unit_tests/vel_controller_tests.cpp
@@ -7,9 +7,9 @@ namespace
   const float some_max_speed = 7.0f;
 }
 
-TEST_CASE("initialised vel_controller outputs zero acc and vel", "[vel_controller_buttons_no_accel]")
+TEST_CASE("initialised vel_controller outputs zero acc and vel", "[vel_controller_flying]")
 {
-  vel_controller_buttons_no_accel vc;
+  vel_controller_flying vc;
 
   REQUIRE(vc.get_vel() == vec2(0, 0));
   REQUIRE(vc.get_acc() == vec2(0, 0));
@@ -20,9 +20,9 @@ TEST_CASE("initialised vel_controller outputs zero acc and vel", "[vel_controlle
   REQUIRE(vc.get_acc() == vec2(0, 0));
 }
 
-TEST_CASE("right button outputs expected vel", "[vel_controller_buttons_no_accel]")
+TEST_CASE("right button outputs expected vel", "[vel_controller_flying]")
 {
-  vel_controller_buttons_no_accel vc;
+  vel_controller_flying vc;
  
   vc.on_dir_button_action(dir_button_action(dir_button_name::right, button_value::down));
 
@@ -33,9 +33,9 @@ TEST_CASE("right button outputs expected vel", "[vel_controller_buttons_no_accel
   REQUIRE(vc.get_vel() == vec2(1, 0)); // default max speed is 1.
 }
 
-TEST_CASE("max_speed scales velocity vec length", "[vel_controller_buttons_no_accel]")
+TEST_CASE("max_speed scales velocity vec length", "[vel_controller_flying]")
 {
-  vel_controller_buttons_no_accel vc;
+  vel_controller_flying vc;
  
   vc.set_max_speed(some_max_speed);
  
@@ -46,17 +46,17 @@ TEST_CASE("max_speed scales velocity vec length", "[vel_controller_buttons_no_ac
   REQUIRE(vc.get_vel() == vec2(some_max_speed, 0)); // vel vec is scaled by max speed
 }
 
-TEST_CASE("left button outputs expected vel", "[vel_controller_buttons_no_accel]")
+TEST_CASE("left button outputs expected vel", "[vel_controller_flying]")
 {
-  vel_controller_buttons_no_accel vc;
+  vel_controller_flying vc;
   vc.on_dir_button_action(dir_button_action(dir_button_name::left, button_value::down));
   vc.update(some_dt_value);
   REQUIRE(vc.get_vel() == vec2(-1, 0));
 }
 
-TEST_CASE("left and right button outputs expected vel", "[vel_controller_buttons_no_accel]")
+TEST_CASE("left and right button outputs expected vel", "[vel_controller_flying]")
 {
-  vel_controller_buttons_no_accel vc;
+  vel_controller_flying vc;
   vc.on_dir_button_action(dir_button_action(dir_button_name::left, button_value::down));
   vc.on_dir_button_action(dir_button_action(dir_button_name::right, button_value::down));
   vc.update(some_dt_value);
