4480afa1db402df61b2c4596bbfe0045638b2366
Juliet Colman <juliet@amjula.com>
2023-06-10 16:51:11 +0100

    Use std::make_unique, not new

--- village/source/init_state.cpp
+++ village/source/init_state.cpp
@@ -12,16 +12,16 @@ void init_state::on_active()
   the_screen->set_size(PRETEND_SCREEN_W, PRETEND_SCREEN_H);
 
   // Init game states
-  the_attract_state.reset(new attract_state);
-  the_enter_hi_score_state.reset(new enter_hi_score_state);
-  the_game_over_state.reset(new game_over_state);
-  the_load_level_state.reset(new load_level_state);
-  the_new_game_state.reset(new new_game_state);
-  the_play_state.reset(new play_state);
-  the_show_hi_scores_state.reset(new show_hi_scores_state);
-  the_splash_state.reset(new splash_state);
+  the_attract_state = std::make_unique<attract_state>();
+  the_enter_hi_score_state = std::make_unique<enter_hi_score_state>();
+  the_game_over_state = std::make_unique<game_over_state>();
+  the_load_level_state = std::make_unique<load_level_state>();
+  the_new_game_state = std::make_unique<new_game_state>();
+  the_play_state = std::make_unique<play_state>();
+  the_show_hi_scores_state = std::make_unique<show_hi_scores_state>();
+  the_splash_state = std::make_unique<splash_state>();
 
-  the_sound_player.reset(new sound_player_bass24);
+  the_sound_player = std::make_unique<sound_player_bass24>();
 
   load_attract_level();
 
